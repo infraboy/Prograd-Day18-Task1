951a8a47ba397ca0ca824f2c252044332ce0f6e9
infraboy <ayushkumarghosh@gmail.com>
2022-07-28 09:15:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import First from './Components/First';
+import Second from './Components/Second';
+import Three from './Components/Three';
+import Four from './Components/Four';
+import Five from './Components/Five';
+
+function App() {
+  return (
+    <div className="App">
+      <First />
+      <Second />
+      <Three />
+      <Five />
+      <Four />
+    </div>
+  );
+}
+
+export default App;
